fc323c6094a6fddf314eb46fa4253a03f5ffe305
Jason McCoubrey <jason.mccoubrey@aquaq.co.uk>
2020-02-05 15:44:51 +0000

    Now accepts data from homer port 5013 and populates table

--- example/lib/pages/web_socket.dart
+++ example/lib/pages/web_socket.dart
@@ -5,113 +5,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/io.dart';
-
-
-Future<List<Result>> fetchResults(http.Client client) async {
-  //final response = await client.get('https://api.myjson.com/bins/j5xau');
-  final response = await client.get('https://api.myjson.com/bins/uxi8a');
-  // Use the compute function to run parseResults in a separate isolate
-  return compute(parseResults, response.body);
-}
-
-// A function that will convert a response body into a List<Result>
-List<Result> parseResults(String responseBody) {
-  //print(responseBody);
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-
-  return parsed.map<Result>((json) => Result.fromJson(json)).toList();
-}
-
-class Result {
-  final String hierarchy;
-  final double position;
-  final double bnlmdt;
-  final double bnltyd;
-  final double mmv;
-  final double epd;
-
-  Result({this.hierarchy, this.position, this.bnlmdt, this.bnltyd, this.mmv, this.epd});
-
-  bool selected = false;
-
-  factory Result.fromJson(Map<String, dynamic> json) {
-    return Result(
-      hierarchy: json['Hierarchy'] as String,
-      position: json['Position'] as double,
-      bnlmdt: json['BNL_MDT'] as double,
-      bnltyd: json['BNL_TYD'] as double,
-      mmv: json['MMV'] as double,
-      epd: json['EPD'] as double,
-    );
-  }
-}
-
-class ResultsDataSource extends DataTableSource {
-  final List<Result> _results;
-  ResultsDataSource(this._results);
-  void _sort<T>(Comparable<T> getField(Result d), bool ascending) {
-    _results.sort((Result a, Result b) {
-      if (!ascending) {
-        final Result c = a;
-        a = b;
-        b = c;
-      }
-      final Comparable<T> aValue = getField(a);
-      final Comparable<T> bValue = getField(b);
-      return Comparable.compare(aValue, bValue);
-    });
-    notifyListeners();
-  }
-
-  int _selectedCount = 0;
-
-  @override
-  DataRow getRow(int index) {
-    assert(index >= 0);
-    if (index >= _results.length) return null;
-    final Result result = _results[index];
-    return DataRow.byIndex(
-        index: index,
-        cells: <DataCell>[
-          DataCell(Text('${result.hierarchy}')),
-          DataCell(Text('${result.position}',
-              style: TextStyle(
-                  color: setColor(result.position),
-                  fontSize: 14))),
-          DataCell(Text('${result.bnlmdt}',
-              style: TextStyle(
-                  color: setColor(result.bnlmdt),
-                  fontSize: 14))),
-          DataCell(Text('${result.bnltyd}',
-              style: TextStyle(
-                  color: setColor(result.bnltyd),
-                  fontSize: 14))),
-          DataCell(Text('${result.mmv}',
-              style: TextStyle(
-                  color: setColor(result.mmv),
-                  fontSize: 14))),
-          DataCell(Text('${result.epd}',
-              style: TextStyle(
-                  color: setColor(result.epd),
-                  fontSize: 14))),
-        ]);
-  }
-
-  @override
-  int get rowCount => _results.length;
-
-  @override
-  bool get isRowCountApproximate => false;
-
-  @override
-  int get selectedRowCount => _selectedCount;
-
-  void _selectAll(bool checked) {
-    for (Result result in _results) result.selected = checked;
-    _selectedCount = checked ? _results.length : 0;
-    notifyListeners();
-  }
-}
+import 'package:example/classes/Result.dart';
 
 class WebSocket extends StatefulWidget {
   ResultsDataSource _resultsDataSource = ResultsDataSource([]);
@@ -142,12 +36,14 @@ class _WebSocketState extends State<WebSocket> {
 
   Future<void> setData(String data) async {
     final results = data;
-    if (!isLoaded) {
+//    if (!isLoaded) {
       setState(() {
-        _resultsDataSource = ResultsDataSource(parseResults(results));
+        List<Result> existingData = _resultsDataSource.getData();
+        existingData.addAll(ResultsDataSource(parseResults(results)).getData());
+        _resultsDataSource = ResultsDataSource(existingData);
         isLoaded = true;
       });
-    }
+//    }
   }
 
 
@@ -155,7 +51,7 @@ class _WebSocketState extends State<WebSocket> {
   initState() {
     super.initState();
     channel5.stream.asBroadcastStream().listen((data) {
-      print("Channel Data Received: ");
+      print("Channel Data Received:");
       print(data);
       setData(data);
     }, onDone: () {
@@ -186,65 +82,122 @@ class _WebSocketState extends State<WebSocket> {
             //onSelectAll: _resultsDataSource._selectAll,
             columns: <DataColumn>[
               DataColumn(
-                  label: const Text('Hierarchy',
+                  label: const Text('Time',
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
                   onSort: (int columnIndex, bool ascending) => _sort<String>(
-                          (Result d) => d.hierarchy, columnIndex, ascending)),
+                          (Result d) => d.time, columnIndex, ascending)),
               DataColumn(
-                  label: const Text('Position',
+                  label: const Text('SYM',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+                  numeric: true,
+                  onSort: (int columnIndex, bool ascending) => _sort<String>(
+                          (Result d) => d.sym, columnIndex, ascending)),
+              DataColumn(
+                  label: const Text('Price',
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
                   numeric: true,
                   onSort: (int columnIndex, bool ascending) => _sort<num>(
-                          (Result d) => d.position, columnIndex, ascending)),
+                          (Result d) => d.price, columnIndex, ascending)),
               DataColumn(
-                  label: const Text('BNL_MDT',
+                  label: const Text('Size',
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
                   numeric: true,
                   onSort: (int columnIndex, bool ascending) => _sort<num>(
-                          (Result d) => d.bnlmdt, columnIndex, ascending)),
+                          (Result d) => d.size, columnIndex, ascending)),
               DataColumn(
-                  label: const Text('BNL_TYD',
+                  label: const Text('Exchange',
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
                   numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>(
-                          (Result d) => d.bnltyd, columnIndex, ascending)),
-              DataColumn(
-                  label: const Text('MMV',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>(
-                          (Result d) => d.mmv, columnIndex, ascending)),
-              DataColumn(
-                  label: const Text('EPD',
-                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  numeric: true,
-                  onSort: (int columnIndex, bool ascending) => _sort<num>(
-                          (Result d) => d.mmv, columnIndex, ascending)),
+                  onSort: (int columnIndex, bool ascending) => _sort<String>(
+                          (Result d) => d.exchange, columnIndex, ascending)),
             ],
             source: _resultsDataSource)
       ]));
-
-//      floatingActionButton: new FloatingActionButton(
-//        child: new Icon(Icons.send),
-//        onPressed: _sendMyMessage,
-//      ),
-//    )
   }
-//
 
-  void _sendMyMessage() {
-    if (editingController.text.isNotEmpty) {
-      streamController.add(editingController.text);
-      channel5.sink.add(editingController.text);
-    }
-  }
 }
-MaterialColor setColor(double num) {
-  if (num == 0) {
-    return Colors.amber;
-  } else if (num < 0) {
-    return Colors.red;
-  } else {
-    return Colors.green;
+
+
+Future<List<Result>> fetchResults(http.Client client) async {
+  //final response = await client.get('https://api.myjson.com/bins/j5xau');
+  final response = await client.get('https://api.myjson.com/bins/uxi8a');
+  // Use the compute function to run parseResults in a separate isolate
+  return compute(parseResults, response.body);
+}
+
+// A function that will convert a response body into a List<Result>
+List<Result> parseResults(String responseBody) {
+  //print(responseBody);
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed[0]["tradetable"].map<Result>((json) => Result.fromJson(json)).toList();
+}
+
+class ResultsDataSource extends DataTableSource {
+  final List<Result> _results;
+  ResultsDataSource(this._results);
+  void _sort<T>(Comparable<T> getField(Result d), bool ascending) {
+    _results.sort((Result a, Result b) {
+      if (!ascending) {
+        final Result c = a;
+        a = b;
+        b = c;
+      }
+      final Comparable<T> aValue = getField(a);
+      final Comparable<T> bValue = getField(b);
+      return Comparable.compare(aValue, bValue);
+    });
+    notifyListeners();
   }
+
+  List<Result> getData(){
+    return _results;
+  }
+
+  int _selectedCount = 0;
+
+  @override
+  DataRow getRow(int index) {
+    assert(index >= 0);
+    if (index >= _results.length) return null;
+    final Result result = _results[index];
+    return DataRow.byIndex(
+        index: index,
+        cells: <DataCell>[
+          DataCell(Text('${result.time}')),
+          DataCell(Text('${result.sym}',
+              style: TextStyle(fontSize: 14))),
+          DataCell(Text('${result.price}',
+              style: TextStyle(
+                  color: setColor(result.price),
+                  fontSize: 14))),
+          DataCell(Text('${result.size}',
+              style: TextStyle(
+                //color: setColor(result.size),
+                  fontSize: 14))),
+          DataCell(Text('${result.exchange}',
+              style: TextStyle(
+                //color: setColor(result.exchange),
+                  fontSize: 14))),
+        ]);
+  }
+
+  @override
+  int get rowCount => _results.length;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedCount;
+
+  MaterialColor setColor(double num) {
+    if (num == 0) {
+      return Colors.amber;
+    } else if (num < 0) {
+      return Colors.red;
+    } else {
+      return Colors.green;
+    }
+}
 }
